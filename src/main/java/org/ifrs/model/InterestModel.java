970553e1e290e42848bdec15b3eb1805b72536fe
Francisco Wolfarth Mossi <francisco.mossi@cwi.com.br>
2022-01-04 19:04:52 -0300

    removing interestedId notnull

--- src/main/java/org/ifrs/model/InterestModel.java
+++ src/main/java/org/ifrs/model/InterestModel.java
@@ -3,7 +3,6 @@ package org.ifrs.model;
 import javax.validation.constraints.NotNull;
 
 public class InterestModel {
-    @NotNull(message = "Campo interestedId é obrigatório")
     public Long interestedId;
     
     @NotNull(message = "Campo announcementId é obrigatório")
